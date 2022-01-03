934f767e4bb334a48401b5bcbff9bf17c4b4c2ea
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-03 19:21:28 +0530

    Add Home component

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import {
   Grid,
   theme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
 // Components
 import Home from './views/Home';
 
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,14 @@
+import { Box, Heading, VStack } from "@chakra-ui/react"
+
 const Home = () => {
 	return (
 		<div>
-			
+			<Box>
+				<Heading as="h1">Cars</Heading>		
+				<VStack spacing="4">
+
+				</VStack>
+			</Box>	
 		</div>
 	)
 }
