6eaafae1cb9a27e1d782b880f180700a6a05481d
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-07 11:24:51 +0530

    Add DefaultLayout & Navbar

---
+++ src/components/Layout/DefaultLayout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+import Header from "../Navbar";
+// import Footer from "../Footer";
+
+export default function DefaultLayout(props: any) {
+  return (
+    <Flex
+      direction="column"
+      align="center"
+	  w="100%"
+      m="0 auto"
+	  bgColor="blue.50"
+	  minHeight="100vh"
+	  color="paragraph"
+      {...props}
+    >
+      <Header />
+	{props.children}
+    {/* <Footer/> */}
+    </Flex>
+  );
+}
