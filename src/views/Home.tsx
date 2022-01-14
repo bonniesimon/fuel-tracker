1fbe27f3e6fd52e59b1cb35284e821b5df74a4d5
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-14 20:44:10 +0530

    UI: improved ui of home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,26 +1,39 @@
-import { Box, Container, Heading, Skeleton, VStack } from "@chakra-ui/react"
-import { useContext } from "react"
-import CarsContext from "../context/CarsContext"
-import { CarsContextProvider } from "../context/CarsContext"
-
+import { Box, Container, Heading, Skeleton, VStack } from "@chakra-ui/react";
+import { useContext } from "react";
+import CarsContext from "../context/CarsContext";
+import { CarsContextProvider } from "../context/CarsContext";
 
 const Home = () => {
-	const {state} = useContext(CarsContext);
-	return (
-			<div>
-				<Box>
-					<Heading as="h1">Cars</Heading>		
-					<VStack spacing="4" align="stretch">
-						<Skeleton isLoaded={state.isCarsFetched} noOfLines={2} my={2} height="xl">
-								{state.cars.map(car => 
-									<Box w="100px" key={car.id}>{car.carName}</Box>	
-									)	
-								}
-						</Skeleton>
-					</VStack>
-				</Box>	
-			</div>
-	)
-}
+    const { state } = useContext(CarsContext);
+    return (
+        <VStack w="80%" minHeight="100%" align="center" justify="center">
+            <Heading as="h1">Cars</Heading>
+            <VStack w="40%" spacing="4" align="stretch">
+                <Skeleton
+                    isLoaded={state.isCarsFetched}
+                    noOfLines={2}
+                    my={2}
+                    height="xl"
+                >
+                    {state.cars.map((car) => (
+                        <Box 
+							key={car.id}
+							bgColor="white"
+							boxShadow="base"
+							rounded="md"
+							p="6"
+							my="2"
+							_hover={{
+								boxShadow: "lg"
+							}}
+						>
+							{car.carName}
+						</Box>
+                    ))}
+                </Skeleton>
+            </VStack>
+        </VStack>
+    );
+};
 
-export default Home
+export default Home;
