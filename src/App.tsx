61199febab20f09d899825f7ccf92bed1c1326a8
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-15 12:42:12 +0530

    Add new theme

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {
     theme,
 } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
+import CustomTheme from "./config/CustomThemeChakra";
 // Components
 import DefaultLayout from "./components/Layout/DefaultLayout";
 import Home from "./views/Home";
@@ -17,7 +18,7 @@ import Car from "./views/Car";
 import { CarsContextProvider } from "./context/CarsContext";
 
 export const App = () => (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={CustomTheme}>
         <CarsContextProvider>
             <DefaultLayout>
                 <Routes>
