ed69cdd40427c039fedbe1b09053e92d84513a22
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-15 12:05:42 +0530

    Add logic to get car FuelEntries in the component

--- src/views/Car.tsx
+++ src/views/Car.tsx
@@ -1,7 +1,33 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import CarsContext, { FuelEntryType, CarType } from "../context/CarsContext";
+
 	const Car = () => {
+		let carid: number;
+		const params = useParams();
+		if(params.carid !== undefined){
+			carid = parseInt(params.carid);	
+		}
+		const {state} = useContext(CarsContext);
+
+		const [carDetail, setCarDetail] = useState<CarType[]>();
+		const [carFuelEntries, setCarFuelEntries] = useState<FuelEntryType[]>();
+
+		useEffect(() => {
+			const fuelEntriesOfCarFromState = state.fuelEntries.filter(fuelEntry=> fuelEntry.carID === carid);
+			setCarFuelEntries(fuelEntriesOfCarFromState);	
+
+			const carDetailsFromState = state.cars.filter(car => car.id === carid);
+			setCarDetail(carDetailsFromState);
+		}, []);
+
+
 		return (
 			<div>
 				<h1>Hey car page here</h1>	
+				{carFuelEntries?.map(fuelEntry =>
+						<h1 key={fuelEntry.carID}>{fuelEntry.amount}</h1>	
+					)}
 			</div>
 		)
 	}
