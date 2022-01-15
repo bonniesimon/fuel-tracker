7058b7e5c720362dc55ec47577f4339a08b637c9
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-15 14:21:55 +0530

    UI: Add box shadow to entries

--- src/components/FuelEntry.tsx
+++ src/components/FuelEntry.tsx
@@ -1,24 +1,49 @@
-import { Box, Circle, Divider, HStack, Spacer, Text, VStack } from "@chakra-ui/react"
-import { FC } from "react"
-import { FuelEntryType } from "../context/CarsContext"
+import {
+    Box,
+    Circle,
+    Divider,
+    HStack,
+    Spacer,
+    Text,
+    VStack,
+} from "@chakra-ui/react";
+import { FC } from "react";
+import { FuelEntryType } from "../context/CarsContext";
 
-const FuelEntry: FC<FuelEntryType> = ({carID, amount, kilometerReading, entryDate, litres, pricePerLitre}) => {
-	return (
-		<VStack p="6" my="5" w="60%" mx="auto" bgColor="brand.backgroundLight">
-			<HStack w="100%" justify="space-between">
-				<Text>{entryDate}</Text>
-				<Text>{kilometerReading} Km</Text>
-			</HStack>
-			<Circle size="125px" bgColor="brand.background">
-				<VStack>
-					<Text fontWeight="bold">₹{amount}</Text>
-					<Divider size="xl"/>
-					<Text fontWeight="bold">{litres} L</Text>
-				</VStack>
-			</Circle>
-			<Text>₹{pricePerLitre} / L</Text>
-		</VStack>
-	)
-}
+const FuelEntry: FC<FuelEntryType> = ({
+    carID,
+    amount,
+    kilometerReading,
+    entryDate,
+    litres,
+    pricePerLitre,
+}) => {
+    return (
+        <VStack
+            p="6"
+            my="5"
+            w="60%"
+            mx="auto"
+            bgColor="brand.backgroundLight"
+            boxShadow="base"
+			_hover={{
+				boxShadow: "2xl"
+			}}
+        >
+            <HStack w="100%" justify="space-between">
+                <Text>{entryDate}</Text>
+                <Text>{kilometerReading} Km</Text>
+            </HStack>
+            <Circle size="125px" bgColor="brand.background">
+                <VStack>
+                    <Text fontWeight="bold">₹{amount}</Text>
+                    <Divider size="xl" />
+                    <Text fontWeight="bold">{litres} L</Text>
+                </VStack>
+            </Circle>
+            <Text>₹{pricePerLitre} / L</Text>
+        </VStack>
+    );
+};
 
-export default FuelEntry
+export default FuelEntry;
