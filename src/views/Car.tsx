b7163bb2667ef3ca893e8ac7d8b607b68da7ec1d
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-15 23:01:02 +0530

    UI: Add Modal. Format code too

--- src/views/Car.tsx
+++ src/views/Car.tsx
@@ -1,57 +1,88 @@
-import { Heading, VStack, Text, Box, Center } from "@chakra-ui/react";
+import {
+    Heading,
+    VStack,
+    Text,
+    Box,
+    Center,
+    Flex,
+    Spacer,
+    IconButton,
+	useDisclosure,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalCloseButton,
+	ModalHeader
+} from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import FuelEntry from "../components/FuelEntry";
 import CarsContext, { FuelEntryType, CarType } from "../context/CarsContext";
 
-	const Car = () => {
-		let carid: number;
-		const params = useParams();
-		if(params.carid !== undefined){
-			carid = parseInt(params.carid);	
-		}
-		const {state} = useContext(CarsContext);
+const Car = () => {
+	const {isOpen, onOpen, onClose} = useDisclosure();
 
-		const [carDetail, setCarDetail] = useState<CarType>();
-		const [carFuelEntries, setCarFuelEntries] = useState<FuelEntryType[]>();
+    let carid: number;
+    const params = useParams();
+    if (params.carid !== undefined) {
+        carid = parseInt(params.carid);
+    }
+    const { state } = useContext(CarsContext);
 
-		useEffect(() => {
-			const fuelEntriesOfCarFromState = state.fuelEntries.filter(fuelEntry=> fuelEntry.carID === carid);
-			const ReversedFuelEntriesOfCarFromState = fuelEntriesOfCarFromState.slice().reverse();
-			setCarFuelEntries(ReversedFuelEntriesOfCarFromState);	
+    const [carDetail, setCarDetail] = useState<CarType>();
+    const [carFuelEntries, setCarFuelEntries] = useState<FuelEntryType[]>();
 
-			const carDetailsFromState = state.cars.filter(car => car.id === carid);
-			setCarDetail(carDetailsFromState[0]);
-		}, []);
+    useEffect(() => {
+        const fuelEntriesOfCarFromState = state.fuelEntries.filter(
+            (fuelEntry) => fuelEntry.carID === carid
+        );
+        const ReversedFuelEntriesOfCarFromState = fuelEntriesOfCarFromState
+            .slice()
+            .reverse();
+        setCarFuelEntries(ReversedFuelEntriesOfCarFromState);
 
+        const carDetailsFromState = state.cars.filter(
+            (car) => car.id === carid
+        );
+        setCarDetail(carDetailsFromState[0]);
+    }, []);
 
-		return (
-			<Box w="80%">
-				<Center my="5">
-				<VStack align="flex-start" justify="center">
-					<Text opacity="0.7">{carDetail?.fuelType}</Text>
-					<Heading>{carDetail?.carName}</Heading>
-				</VStack>
-				</Center>
-				{ carFuelEntries && carFuelEntries?.length > 0 ? 
-				carFuelEntries?.map(fuelEntry =>
-						<FuelEntry
-							carID={fuelEntry.carID}
-							entryDate={fuelEntry.entryDate}
-							amount={fuelEntry.amount}
-							kilometerReading={fuelEntry.kilometerReading}
-							litres={fuelEntry.litres}
-							pricePerLitre={fuelEntry.pricePerLitre}
-						/>
-					)
-				:
-					<Center>
-						<Text>No Entries to List</Text>
-					</Center>
-				}
-			</Box>
-		)
-	}
-	
-	export default Car
-	
+    return (
+        <Box w="80%">
+            <Flex w="60%" mx="auto" my="9" align="center">
+                <VStack align="flex-start" justify="center">
+                    <Text opacity="0.7">{carDetail?.fuelType}</Text>
+                    <Heading>{carDetail?.carName}</Heading>
+                </VStack>
+                <Spacer />
+                <IconButton onClick={onOpen} aria-label="Add fuel entry" icon={<AddIcon />} />
+            </Flex>
+            {carFuelEntries && carFuelEntries?.length > 0 ? (
+                carFuelEntries?.map((fuelEntry) => (
+                    <FuelEntry
+                        carID={fuelEntry.carID}
+                        entryDate={fuelEntry.entryDate}
+                        amount={fuelEntry.amount}
+                        kilometerReading={fuelEntry.kilometerReading}
+                        litres={fuelEntry.litres}
+                        pricePerLitre={fuelEntry.pricePerLitre}
+                    />
+                ))
+            ) : (
+                <Center>
+                    <Text>No Entries to List</Text>
+                </Center>
+            )}
+			<Modal isOpen={isOpen} onClose={onClose} isCentered>
+				<ModalOverlay/>
+				<ModalContent>
+					<ModalHeader>Add Fuel Entry</ModalHeader>
+					<ModalCloseButton/>
+				</ModalContent>
+			</Modal>
+        </Box>
+    );
+};
+
+export default Car;
