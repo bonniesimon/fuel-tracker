1849cbc9d3934d89027a5b1dae19918113d76068
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-19 23:49:41 +0530

    Add formatting data to FuelEntryType in the fetcher function for swr
    Doing this might not be the best idea. It started causing some issues. I
    should try something else maybe later.

--- src/utils/fetchers.ts
+++ src/utils/fetchers.ts
@@ -1,11 +1,17 @@
 import { convertApiDataToFuelType } from "./serialize";
 
-const fetchFuelEntryByCarID = async (url: string) => {
+const fetchFuelEntryByCarID = async (url: string): Promise<any>=> {
 	const res = await fetch(url);
 	// const json = await res.json();
 	// console.log(json);
 	// return convertApiDataToFuelType(json);
-	return await res.json();
+	// return await res.json();
+	const json = await res.json();
+	const formattedData = convertApiDataToFuelType(json);
+	console.log(formattedData);
+	return new Promise((resolve, reject) => {
+		resolve(formattedData);
+	})
 }
 
 export {fetchFuelEntryByCarID};
