06922ace362edb53f880f21a4365591628536eb4
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-20 20:44:21 +0530

    Remove useEffect and data fetching in Context file

--- src/context/CarsContext.tsx
+++ src/context/CarsContext.tsx
@@ -1,7 +1,5 @@
-import { createContext, FC, useEffect, useReducer } from "react";
+import { createContext, FC, useReducer } from "react";
 import CarsReducer from "../reducers/CarsReducer";
-import config from "../config/config";
-import {convertApiDataToCarType} from "../utils/serialize";
 
 type Fuel = "Diesel" | "Petrol";
 
@@ -40,30 +38,6 @@ const CarsContext = createContext<{state: CarsStateType, dispatch: Function}>({
 
 const CarsContextProvider: FC = ({children}) => {
 	const [state, dispatch] = useReducer(CarsReducer, initialState);
-
-
-	useEffect(() => {
-		const getCarsFromAPI = async (url: string) => {
-			try{
-				const res = await fetch(url);
-				let results = await res.json();
-
-				/**
-				 * Done to convert the api data to the CarType
-				 */
-				const carDataFormated: CarType[] = convertApiDataToCarType(results);
-
-				dispatch({
-					type: "FETCH_CARS_SUCCESS",
-					payload: carDataFormated
-				 })
-			}catch(e: any){
-				console.error("Error", e);
-			}
-		}
-
-		getCarsFromAPI(`${config.backendUrl}/api/car/all`);
-	}, []);	
 	
 	return(
 		<CarsContext.Provider value={{state, dispatch}}>
