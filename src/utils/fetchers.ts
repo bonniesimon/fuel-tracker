fa6fe2a01eba1f9ee714b9523ab14ff5bebe1a9a
Bonnie Simon <bonnie.developer@gmail.com>
2022-01-20 20:42:46 +0530

    Add fetcher for getAllCars

--- src/utils/fetchers.ts
+++ src/utils/fetchers.ts
@@ -1,11 +1,7 @@
-import { convertApiDataToFuelType } from "./serialize";
+import { convertApiDataToCarType, convertApiDataToFuelType } from "./serialize";
 
 const fetchFuelEntryByCarID = async (url: string): Promise<any>=> {
 	const res = await fetch(url);
-	// const json = await res.json();
-	// console.log(json);
-	// return convertApiDataToFuelType(json);
-	// return await res.json();
 	const json = await res.json();
 	const formattedData = convertApiDataToFuelType(json);
 	return new Promise((resolve, reject) => {
@@ -13,4 +9,13 @@ const fetchFuelEntryByCarID = async (url: string): Promise<any>=> {
 	})
 }
 
-export {fetchFuelEntryByCarID};
+const fetchAllCars = async (url: string): Promise<any> => {
+	const res = await fetch(url);
+	const json = await res.json();
+	const formattedData = convertApiDataToCarType(json);
+	return new Promise((resolve, reject) => {
+		resolve(formattedData);
+	})
+}
+
+export {fetchFuelEntryByCarID, fetchAllCars};
