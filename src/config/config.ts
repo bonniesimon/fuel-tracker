b99bae5b20f5f58b5f5d88e82b15eb4ef7ed287f
Bonnie Simon <bonnie.developer@gmail.com>
2022-03-03 17:41:11 +0530

    Update config to consume info from .env file
    Instead of using a config.json, we now get secret data from .env. Using
    .env is much more easier for hosting it in netlify. I can provide a
    NODE_ENV variable in netlify and set it to production to make the app
    use the live API url.

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,26 @@
-import config from './config.json';
+// import config from './config.json';
+
+const API_URL_DEV = process.env.REACT_APP_API_URL_DEV;
+const API_URL_PROD = process.env.REACT_APP_API_URL_PROD;
+
+const NODE_ENV = process.env.NODE_ENV;
+
+let url;
+switch(NODE_ENV){
+	case "production":
+		url = API_URL_PROD;
+		break;
+	case "development":
+		url = API_URL_DEV;
+		break;
+	default:
+		break;
+}
+const API_URL = url;
+
+const config = {
+	API_URL
+};
+
 
 export default config;
